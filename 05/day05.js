dddaed500a3df4f403984b4638151fb19defad41
Dominika Szymańska <dominika1872@gmail.com>
2021-12-07 01:18:46 +0100

    Add day05

---
+++ 05/day05.js
@@ -0,0 +1,46 @@
+import { readFileString } from '../functions.js'
+
+let input = readFileString('05/day05_input.txt').map(line => line.split(' -> '))
+input = input.map(pair => pair.map(point => point.split(',')))
+
+let p = {
+  points: [],
+  overlap: []
+}
+
+const findPoint = point => {
+  if (
+    p.points.some(el => el[0] === point[0] && el[1] === point[1]) &&
+    !p.overlap.some(el => el[0] === point[0] && el[1] === point[1])
+  ) {
+    p.overlap.push(point)
+  } else {
+    p.points.push(point)
+  }
+}
+
+input.forEach(pair => {
+  const x1 = +pair[0][0]
+  const x2 = +pair[1][0]
+  const y1 = +pair[0][1]
+  const y2 = +pair[1][1]
+  if (x1 === x2 || y1 === y2) {
+    const distX = Math.abs(x2 - x1)
+    const distY = Math.abs(y2 - y1)
+    let point = []
+
+    if (distX > distY) {
+      for (let i = 0; i <= distX; i++) {
+        x1 < x2 ? (point = [x1 + i, y1]) : (point = [x2 + i, y1])
+        findPoint(point)
+      }
+    } else {
+      for (let i = 0; i <= distY; i++) {
+        y1 < y2 ? (point = [x1, y1 + i]) : (point = [x2, y2 + i])
+        findPoint(point)
+      }
+    }
+  }
+})
+
+console.log(p.overlap.length)

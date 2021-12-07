99076ae45f4fc2f9e46cb32af291fdd94bdbad28
Dominika Szymańska <dominika1872@gmail.com>
2021-12-07 12:39:29 +0100

    Add day07

---
+++ 07/day07.js
@@ -0,0 +1,28 @@
+import { readFileString } from '../functions.js'
+
+const input = readFileString('07/day07_input.txt')[0]
+  .split(',')
+  .map(pos => +pos)
+
+input.sort((a, b) => a - b)
+const median = input[input.length / 2]
+
+let fuel = 0
+input.forEach(crab => (fuel += Math.abs(median - crab)))
+
+console.log(fuel)
+
+let average = Math.floor(input.reduce((a, b) => a + b) / input.length)
+console.log(average)
+console.log()
+fuel = 0
+input.forEach(crab => {
+  const diff = Math.abs(average - crab)
+  let crabFuel = 0
+  for (let i = 1; i <= diff; i++) {
+    crabFuel += i
+  }
+  fuel += crabFuel
+})
+
+console.log(fuel)
